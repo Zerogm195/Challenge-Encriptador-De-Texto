f48d2ca6a4aeaa51bd09a14e460e6043de4cfed5
Zuro <Luisdelahoz84@gmail.com>
2024-07-08 17:54:14 -0500

    Se añadieron las funciones principales de encriptar y desencriptar texto y copiar

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 //Encriptador
+    function validar(txtval) {
+
+        const valido = /^[a-z\s]*$/.test(txtval);
+    
+        if (!valido) {
+
+        return false;
+
+        } else {
+
+        return true;
+
+        }
+    }
 
     function Encriptar(){
 
@@ -6,14 +20,24 @@
 
         let p = document.getElementById("Parrafo");
 
+        //Validar
+
+        if (!validar(input)) {
+
+            p.innerHTML = "El texto que ingresó no es valido; Por favor intentelo denuevo."
+
+            return;
+
+        }
+
         //p que seria "Parrafo" imprime el input del usuario
         //replace reemplaza la cadena
 
-        p.innerHTML = input.replace(/e/g, "enter")
-                        .replace(/i/g, "imes")
-                        .replace(/a/g, "ai")
-                        .replace(/o/g, "ober")
-                        .replace(/u/g, "ufat");
+        p.innerHTML = "Su texto encriptado es: "+ input.replace(/e/g, "enter")
+                                                       .replace(/i/g, "imes")
+                                                       .replace(/a/g, "ai")
+                                                       .replace(/o/g, "ober")
+                                                       .replace(/u/g, "ufat");
 
     }
 
@@ -24,11 +48,34 @@
         let input = document.getElementById("Solicitud").value;
 
         let p = document.getElementById("Parrafo");
+
+        //Validar
+
+        if (!validar(input)) {
+
+            p.innerHTML = "El texto que ingresó no es valido; Por favor intentelo denuevo."
+
+            return;
+
+        }
         
-        p.innerHTML = input.replace(/enter/g, "e")
-                        .replace(/imes/g, "i")
-                        .replace(/ai/g, "a")
-                        .replace(/ober/g, "o")
-                        .replace(/ufat/g, "u");
+        p.innerHTML = "Su texto desencriptado es: " + input.replace(/enter/g, "e")
+                                                           .replace(/imes/g, "i")
+                                                           .replace(/ai/g, "a")
+                                                           .replace(/ober/g, "o")
+                                                           .replace(/ufat/g, "u");
+
+    }
+
+//Copiar texto
+
+    function CopiarTexto(){
+
+    
+        let p = document.getElementById("Parrafo").innerHTML;
+
+
+        navigator.clipboard.writeText(p = p.replace("Su texto encriptado es: ", ""));
+
 
     }
