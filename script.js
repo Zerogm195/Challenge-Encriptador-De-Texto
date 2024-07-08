02715d7d3d7db4ce626ddb64a1d55fcf067b681b
Zuro <Luisdelahoz84@gmail.com>
2024-07-08 16:01:16 -0500

    Se agregan clases a html y se añade la funcionalidad del encriptador del texto en js

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+//Encriptador
 
-function Encriptar(){
+    function Encriptar(){
 
-    let input = document.getElementById("Solicitud").value;
-    let s = document.getElementById("parrafo");
-    s.innerHTML = input;
-    
-    
+        let input = document.getElementById("Solicitud").value;
 
+        let p = document.getElementById("Parrafo");
 
-}
+        //p que seria "Parrafo" imprime el input del usuario
+        //replace reemplaza la cadena
+
+        p.innerHTML = input.replace(/e/g, "enter")
+                        .replace(/i/g, "imes")
+                        .replace(/a/g, "ai")
+                        .replace(/o/g, "ober")
+                        .replace(/u/g, "ufat");
+
+    }
+
+//Desencriptador
+
+    function Desencriptar(){
+
+        let input = document.getElementById("Solicitud").value;
+
+        let p = document.getElementById("Parrafo");
+        
+        p.innerHTML = input.replace(/enter/g, "e")
+                        .replace(/imes/g, "i")
+                        .replace(/ai/g, "a")
+                        .replace(/ober/g, "o")
+                        .replace(/ufat/g, "u");
+
+    }
